369661062c1e34b860b60597f35deb11a719292b
Pavani Immadisetti <pavaniimmadisetty6@gmail.com>
2026-02-28 10:37:15 +0530

    Remove codexfms-1 folder

--- codexfms-1/backend/src/config/constants.js
+++
@@ -1,7 +0,0 @@
-﻿const SESSION_COOKIE_NAME = process.env.SESSION_COOKIE_NAME || 'sid';
-const SESSION_TTL_HOURS = Number(process.env.SESSION_TTL_HOURS || 12);
-
-module.exports = {
-  SESSION_COOKIE_NAME,
-  SESSION_TTL_HOURS
-};
--- codexfms-1/backend/src/utils/session.js
+++
@@ -1,9 +0,0 @@
-﻿const crypto = require('crypto');
-
-function generateSessionId() {
-  return crypto.randomBytes(48).toString('hex');
-}
-
-module.exports = {
-  generateSessionId
-};
